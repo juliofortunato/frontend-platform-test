3f25a64ddbdf405ae8c864f3ecad4e2e450e2f11
Júlio Fortunato <juliofortunato58@gmail.com>
2024-10-04 08:43:35 -0300

    ✨ feat: add initial title component to the home page

--- app/(routes)/(home)/page.tsx
+++ app/(routes)/(home)/page.tsx
@@ -1,5 +1,24 @@
+import FavoritesButton from "@/app/_components/FavoritesButton";
 import Header from "../../_components/Header";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+
+      <div className="container mx-auto flex flex-col gap-10">
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-3">
+            <div className="flex items-center gap-2">
+              <h1 className="text-[2rem] font-medium">Your Library</h1>
+              <FavoritesButton />
+            </div>
+            <p className="font-medium text-white/50">
+              You have 10 songs in your library
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
